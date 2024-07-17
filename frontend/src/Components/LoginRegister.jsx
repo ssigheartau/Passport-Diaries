d5aaae10dc0b8ee6d267caf3f6d9f7b951619644
ssigheartau <ssigheartau@yahoo.com>
2024-07-17 00:53:57 -0700

    Rendered mapbox and currently working on Login

---
+++ frontend/src/Components/LoginRegister.jsx
@@ -0,0 +1,29 @@
+// import React from 'react';
+// import './LoginRegister.css';
+
+// const LoginRegister = () => {
+//     return (
+//         <div className='wrapper'>
+//             <div className="form-box login">
+//                 <form action="">
+//                     <h1>Login</h1>
+//                     <div className="input_box">
+//                         <input type="text" placeholder='Username'required/>
+//                     </div>
+//                     <div className="input_box">
+//                         <input type="password" placeholder='Password'required/>
+//                     </div>
+//                     <div className="remember-forgot">
+//                         <label><input type="checkbox" />Remember me</label>
+//                         <a href="#">Forgot password?</a>
+//                     </div>
+//                     <button type="submit">Login</button>
+//                     <div className="register-link"></div>
+//                     <p>Don't have an account? <a href="#">Register</a></p>
+//                 </form>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default LoginRegister; 
---
+++ frontend/src/env.sh
@@ -0,0 +1,1 @@
+export VITE_MAPBOX_ACCESS_TOKEN="pk.eyJ1Ijoic3NpZ2hlYXJ0YXUiLCJhIjoiY2x5aWFzZzEyMGU2dzJqcTIxd2t6b3NxZyJ9.7_hqfaBl-JxBimyE9nDwcQ"
